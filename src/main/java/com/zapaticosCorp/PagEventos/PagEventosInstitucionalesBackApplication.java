a55d439cad8c6b1c094ee72cb0f3af44b994c3ca
Edmh <edavidmanotas@gmail.com>
2025-04-07 10:34:06 -0500

    fix: builder class and pomx

--- src/main/java/com/zapaticosCorp/PagEventos/PagEventosInstitucionalesBackApplication.java
+++ src/main/java/com/zapaticosCorp/PagEventos/PagEventosInstitucionalesBackApplication.java
@@ -2,12 +2,21 @@ package com.zapaticosCorp.PagEventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PagEventosInstitucionalesBackApplication {
+public class PagEventosInstitucionalesBackApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PagEventosInstitucionalesBackApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PagEventosInstitucionalesBackApplication.class, args);
 	}
 
+
 }
+
